d95ca3fbc0e626aa1d361c04e10e4582c664f9f7
riyad899 <raditkhanyt@gmail.com>
2026-05-16 01:50:46 +0600

    Mount all missing API routes: catalog, brand, blog, coupon, service-center, job

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -5,6 +5,12 @@ import { LogoRoute } from '../module/Logo/logo.route.js';
 import { CustomerManagementRoute } from '../module/CustomerManagement/customerManagement.route.js';
 import { HomeSettingRoute } from '../module/HomeSetting/homeSetting.route.js';
 import { StaffRoute } from '../module/Staff/staff.route.js';
+import { CatalogRoute } from '../module/Catalog/catalog.route.js';
+import { BrandRoute } from '../module/Brand/brand.route.js';
+import { BlogRoute } from '../module/Blog/blog.route.js';
+import { CouponRoute } from '../module/Coupon/coupon.route.js';
+import { ServiceCenterRoute } from '../module/ServiceCenter/serviceCenter.route.js';
+import { JobRoute } from '../module/Job/job.route.js';
 
 const router = Router();
 
@@ -14,5 +20,11 @@ router.use("/logo", LogoRoute);
 router.use("/home-setting", HomeSettingRoute);
 router.use("/customer-management", CustomerManagementRoute);
 router.use("/staff", StaffRoute);
+router.use("/", CatalogRoute);
+router.use("/brand", BrandRoute);
+router.use("/", BlogRoute);
+router.use("/", CouponRoute);
+router.use("/", ServiceCenterRoute);
+router.use("/", JobRoute);
 
 export const IndexRoute = router;
